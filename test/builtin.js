1acccd60798bd8191d307ec4e54b151b298b9fc5
Dana Burkart <dana.burkart@gmail.com>
2014-03-07 00:15:09 -0800

    Adds builtin module
    Adds the following builtin functions:
    
     - sqrt(x)
     - abs(x)
     - round(x)
    
    Also exposes a failed unit test.

---
+++ test/builtin.js
@@ -0,0 +1,20 @@
+var should = require('should');
+var Parser = require('../lib/parser');
+var Lexer = require('../lib/lexer');
+
+describe('Testing builtin', function() {
+	it('sqrt(16) is 4', function() {
+		var parser = new Parser('sqrt(16)');
+		parser.eval().should.equal(4);
+	});
+
+	it('abs(-4) is 4', function() {
+		var parser = new Parser('abs(-4)');
+		parser.eval().should.equal(4);
+	});
+
+	it('round(2.6) is 3', function() {
+		var parser = new Parser('round(2.6)');
+		parser.eval().should.equal(3);
+	});
+});
